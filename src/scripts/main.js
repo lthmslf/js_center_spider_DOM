f78166ba0904404310efa534848fcd6d714a1555
Vladyslav <unadvisedmail@gmail.com>
2025-03-25 23:49:02 +0100

    solution another one

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const wall = document.querySelector('.wall').getBoundingClientRect();
 const spider = document.querySelector('.spider');
 
-spider.style.top = `${50}%`;
-spider.style.left = `${50}%`;
-spider.style.transform = 'translate(-50%, -50%)';
+spider.getBoundingClientRect();
+
+spider.style.top = `${wall.height / 2 - spider.height / 2}px`;
+spider.style.left = `${wall.width / 2 - spider.width / 2}px`;
